b7bb7b336a54796b4098079eb654fb4e41435d07
lcpo <po.luk@northeastern.edu>
2023-10-05 14:31:42 -0400

    home page kanbas

---
+++ public/notes/2023-10-05/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import"/vendors/bootstrap/css/bootstrap.min.css";
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+reportWebVitals();
