1943586c55844458deec1db2b79075affceb400d
lcpo <po.luk@northeastern.edu>
2023-11-27 13:14:49 -0500

    project updates

---
+++ src/project/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import * as client from "./napster-service";
+import Nav from "./nav";
+
+function Project() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Project;
